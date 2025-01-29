ff2e0ff2eb17ad5fbdc1210b45653092bcf70bde
shubhamrakh <shubham.rakh35@gmail.com>
2025-01-29 23:58:42 +0530

    Add files via upload

---
+++ employee-management/src/main/java/com/employee/management/service/EmployeeService.java
@@ -0,0 +1,57 @@
+package com.employee.management.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.employee.management.model.Employee;
+import com.employee.management.repository.EmployeeRepository;
+
+// employee service class
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// fetching all employees
+	public List<Employee> getAllEmployees(){
+		List<Employee> emps = (List<Employee>)employeeRepository.findAll(); 
+		return emps;
+	}
+	
+	// fetching employee by id
+	public Optional<Employee> getEmployee(int id){
+		return employeeRepository.findById(id);
+	}
+	
+	// inserting employee
+	public void addEmployee(Employee e) {
+		employeeRepository.save(e);
+	}
+	
+	// updating employee by id
+	public void updateEmployee(Employee emp, int id){
+		if(id == emp.getEmployeeID()) {
+			employeeRepository.save(emp);
+		}
+	}
+	
+	// deleting all employees
+	public void deleteAllEmployees(){
+		employeeRepository.deleteAll();
+	}
+	
+	// deleting employee by id
+	public void deleteEmployeeByID(int id){
+		employeeRepository.deleteById(id);
+	}
+	
+	//patching/updating employee by id
+	public void patchEmployee(Employee emp, int id) {
+		if(id == emp.getEmployeeID()) {
+			employeeRepository.save(emp);
+		}
+	}
+}
